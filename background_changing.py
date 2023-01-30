6b034d5843700d635820852acf67228835310fdb
Rithwik <amaraneni.rithwik@gmail.com>
2023-01-30 19:45:26 +0530

    updated the code

--- background_changing.py
+++ background_changing.py
@@ -2,41 +2,33 @@ import cv2
 import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 import os
-
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-# cap.set(cv2.CAP_PROP_FPS, 60)
-
 segmentor = SelfiSegmentation()
-#fpsReader = cvzone.FPS()
-
-# imgBG = cv2.imread("BackgroundImages/3.jpg")
-
-listImg = os.listdir("BackgroundImages")
+listImg = os.listdir("Backgrounds")
 imgList = []
 for imgPath in listImg:
-    img = cv2.imread(f'BackgroundImages/{imgPath}')
+    img = cv2.imread(f'Backgrounds/{imgPath}')
     imgList.append(img)
 
 indexImg = 0
 
 while True:
     success, img = cap.read()
-    # imgOut = segmentor.removeBG(img, (255,0,255), threshold=0.83)
     imgOut = segmentor.removeBG(img, imgList[indexImg], threshold=0.8)
-
     imgStack = cvzone.stackImages([img, imgOut], 2,1)
-    #_, imgStack = fpsReader.update(imgStack)
-    #print(indexImg)
     cv2.imshow("Background_changer", imgStack)
     key = cv2.waitKey(1)
-    if key == ord('a'):
+    #press 'd' for the next background
+    if key == ord('d'):
         if indexImg>0:
             indexImg -=1
-    elif key == ord('d'):
+    #press 'a' for the previous background
+    elif key == ord('a'):
         if indexImg<len(imgList)-1:
             indexImg +=1
+    #press q to stop the execution
     elif key == ord('q'):
         cv2.destroyAllWindows()
         break
