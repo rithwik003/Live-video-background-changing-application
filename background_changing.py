8fd11275f7334124b8216cb0824f8e5ec50da4b1
Rithwik <amaraneni.rithwik@gmail.com>
2023-08-08 17:28:33 +0530

    Update background_changing.py

--- background_changing.py
+++ background_changing.py
@@ -2,33 +2,39 @@ import cv2
 import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 import os
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-segmentor = SelfiSegmentation()
-listImg = os.listdir("Backgrounds")
-imgList = []
-for imgPath in listImg:
-    img = cv2.imread(f'Backgrounds/{imgPath}')
-    imgList.append(img)
 
-indexImg = 0
+video_capture = cv2.VideoCapture(0)
+video_capture.set(3, 640)
+video_capture.set(4, 480)
+segmentor = SelfiSegmentation()
+
+background_images = os.listdir("backgrounds")
+background_list = []
+for background_path in background_images:
+    background = cv2.imread(f"backgrounds/{background_path}")
+    background_list.append(background)
+
+current_background = 0
 
 while True:
-    success, img = cap.read()
-    imgOut = segmentor.removeBG(img, imgList[indexImg], threshold=0.8)
-    imgStack = cvzone.stackImages([img, imgOut], 2,1)
-    cv2.imshow("Background_changer", imgStack)
+    success, frame = video_capture.read()
+    output_frame = segmentor.removeBG(frame, background_list[current_background], threshold=0.8)
+    stack_frames = cvzone.stackImages([frame, output_frame], 2, 1)
+    cv2.imshow("Background Changer", stack_frames)
+
     key = cv2.waitKey(1)
-    #press 'd' for the next background
+
+    # Press 'd' for the next background
     if key == ord('d'):
-        if indexImg>0:
-            indexImg -=1
-    #press 'a' for the previous background
+        if current_background > 0:
+            current_background -= 1
+
+    # Press 'a' for the previous background
     elif key == ord('a'):
-        if indexImg<len(imgList)-1:
-            indexImg +=1
-    #press q to stop the execution
+        if current_background < len(background_list) - 1:
+            current_background += 1
+
+    # Press 'q' to stop the execution
     elif key == ord('q'):
         cv2.destroyAllWindows()
         break
